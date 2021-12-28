547d7effa89d395a11b87096d6493cc0604b72c2
Archakov Dennis <amonbower@gmail.com>
2021-12-29 01:52:40 +0300

    fix

--- controllers/Post.js
+++ controllers/Post.js
@@ -76,11 +76,25 @@ module.exports.show = async (req, res) => {
     res.status(400).json({ error: "Неверный ID записи" });
   } else {
     try {
-      const result = await Post.findById(id).populate("user");
-      if (result) {
-        return res.status(200).json(result);
-      }
-      return res.status(404).json({ error: "Такой записи нет в базе" });
+      Post.findOneAndUpdate(
+        { _id: id },
+        { $inc: { views: 1 } },
+        { new: true },
+        async (err) => {
+          if (err) {
+            return res.status(500).json({ errors: err });
+          }
+
+          console.log(123123);
+          const result = await Post.findById(id).populate("user");
+
+          if (result) {
+            return res.status(200).json(result);
+          }
+
+          res.status(404).json({ error: "Такой записи нет в базе" });
+        }
+      );
     } catch (err) {
       console.log(err);
       return res.status(500).json({ error: "Произошла серверная ошибка" });

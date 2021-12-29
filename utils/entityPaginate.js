07c1520fbb05a42b7c10746eb64da4bfdf15b687
Archakov Dennis <amonbower@gmail.com>
2021-12-29 23:33:53 +0300

    fix

--- utils/entityPaginate.js
+++ utils/entityPaginate.js
@@ -1,47 +1,48 @@
 module.exports.entityPaginate = async (entity, req) => {
   const page = Number(req.query.page) || 1;
   const limit = Number(req.query.limit) || 5;
-  const orderBy = req.query.orderBy === "asc" ? "createdAt" : "-createdAt";
+  const userId = req.query.userId;
+  const orderBy = req.query.orderBy === 'asc' ? 'createdAt' : '-createdAt';
 
   const searchArr = {
     Post: [
       {
-        title: new RegExp(req.query.query, "i"),
+        title: new RegExp(req.query.query, 'i'),
       },
       {
-        text: new RegExp(req.query.query, "i"),
+        text: new RegExp(req.query.query, 'i'),
       },
     ],
     Comment: [
       {
-        text: new RegExp(req.query.query, "i"),
+        text: new RegExp(req.query.query, 'i'),
       },
     ],
     User: [
       {
-        fullName: new RegExp(req.query.query, "i"),
+        fullName: new RegExp(req.query.query, 'i'),
       },
       {
-        email: new RegExp(req.query.query, "i"),
+        email: new RegExp(req.query.query, 'i'),
       },
     ],
   };
 
-  const total = await entity
-    .countDocuments({
-      $or: searchArr[entity.modelName],
-    })
-    .exec();
-
   return {
-    total,
+    total: await entity
+      .countDocuments({
+        ...(userId ? { user: userId } : {}),
+        $or: searchArr[entity.modelName],
+      })
+      .exec(),
     items: await entity
       .find({
+        ...(userId ? { user: userId } : {}),
         $or: searchArr[entity.modelName],
       })
       .limit(limit)
       .skip(limit * (page - 1))
       .sort(orderBy)
-      .populate(entity.modelName === "User" ? "" : "user"),
+      .populate(entity.modelName === 'User' ? '' : 'user'),
   };
 };
